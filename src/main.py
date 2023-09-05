eae118a4c7ea77a35b971005fc309ff957db9cd9
JSDulai <jsinghdulai@gmail.com>
2023-09-05 14:08:07 +0200

    Kommentare angepasst

--- src/main.py
+++ src/main.py
@@ -12,12 +12,12 @@ def main_binary_classification(data_path, class_names, num_classes, output_filen
     data = prepare_data(data_path, class_names, num_classes)
     train, test = split_data(data)
     
-    # Create and train the model
+    
     model = create_binary_model()
     model.summary()
-    hist = model.fit(train, epochs=3, validation_data=test)  # Reduced epochs for testing
+    hist = model.fit(train, epochs=3, validation_data=test)  
     
-    # Predict and save results
+    
     save_predictions_to_csv(model, data_path, output_filename, binary=True)
 
 def main_multiclass_classification(data_path, class_names, num_classes, output_filename='predictions_multiclass.csv'):
@@ -29,7 +29,7 @@ def main_multiclass_classification(data_path, class_names, num_classes, output_f
     data = prepare_data(data_path, class_names, num_classes)
     train, val, test = split_data(data)
     
-    # Create and train the model
+    
     model = revised_cnn_model_create() 
     model.summary()
     hist = model.fit(train, epochs=4, validation_data=val)  # Reduced epochs for testing
@@ -39,7 +39,7 @@ def main_multiclass_classification(data_path, class_names, num_classes, output_f
     print(f"Test Accuracy: {accuracy}")
 
     loaded_model = tf.keras.models.load_model("../EchoWatch/models/pt500_model.h5")
-    # Predict and save results
+    
     save_predictions_to_csv(model, data_path, output_filename, binary=False)
 
 def save_predictions_to_csv(model, data_path, output_filename, binary=True):
@@ -60,7 +60,7 @@ def save_predictions_to_csv(model, data_path, output_filename, binary=True):
             print(predicted_class)
             results[file] = predicted_class
 
-    # Save results to CSV
+    
     with open(output_filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['Dateiname', 'Vorhersage'])  
@@ -70,15 +70,13 @@ def save_predictions_to_csv(model, data_path, output_filename, binary=True):
 
 
 def predict_with_saved_model(model_path , wav_file_path):
-    # Load the saved model
+    
     loaded_model = tf.keras.models.load_model(model_path)
     
-    # Process the WAV file
     wav = load_wav_16k_mono(wav_file_path)
     processed_wav = preprocess_wav_for_model(wav)
     processed_wav_batched = tf.expand_dims(processed_wav, axis=0)
     
-    # Make predictions
     predictions = loaded_model.predict(processed_wav_batched)
     
     # Return the class with the highest probability
@@ -87,7 +85,7 @@ def predict_with_saved_model(model_path , wav_file_path):
     predicted_class = tf.argmax(predictions, axis=1).numpy()[0]
     return print(predicted_class)
 
-# Example usage
+
 data_path = os.path.join('data', 'PT500')
 class_names=['A', 'B', 'C', 'D']
 num_classes=4
