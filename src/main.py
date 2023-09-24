05a0ee601b1f1a2389bd47a7efcb4ba747a82dce
JSDulai <jsinghdulai@gmail.com>
2023-09-25 00:37:34 +0200

    Code gesäubert und sortiert

--- src/main.py
+++ src/main.py
@@ -1,38 +1,44 @@
-
 import os
-import csv
 import sys
 sys.path.append("../EchoWatch")
 import tensorflow as tf
-from models.model import create_model,revised_cnn_model_create, piczak_modell
-from utils.utilities import load_audio, preprocess_wav_for_model, get_input_shape_from_data, predict_from_wav, load_wav_16k_mono, live_audio_classification
-from utils.preparation import prepare_data, split_data, prepare_data1
+from models.model import model_dropout, model_optimized, modell_piczak, simple_model
+from utils.utilities import save_predictions_to_csv, preprocess_wav_for_model, load_wav_16k_mono, live_audio_classification, split_and_save_audio_chunks, predict_with_saved_model
+from utils.preparation import prepare_data, split_data
 
-def main_binary_classification(data_path, class_names, num_classes, output_filename='predictions_binary.csv'):
+
+def main_binary_classification(data_path, class_names, num_classes, acti_func ='sigmoid', loss_func = 'BinaryCrossentropy', output_filename='predictions_binary.csv'):
     data = prepare_data(data_path, class_names, num_classes)
     train, test = split_data(data)
     
     
-    model = create_binary_model()
+    model = simple_model(num_classes, acti_func, loss_func)
     model.summary()
     hist = model.fit(train, epochs=3, validation_data=test)  
     
     
     save_predictions_to_csv(model, data_path, output_filename, binary=True)
 
-def main_multiclass_classification(data_path, class_names, num_classes, output_filename='predictions_multiclass.csv'):
+def main_multiclass_classification(data_path, class_names, num_classes, acti_func ='softmax', loss_func = 'CategoricalCrossentropy', output_filename='predictions_multiclass.csv'):
+    
+    #split_and_save_audio_chunks("../EchoWatch/data/F_1500.wav", "PT500")
+    
     
     #predict_with_saved_model(model_path = "../EchoWatch/models/pt500_model.h5", wav_file_path="../EchoWatch/data/PT500/C_1000_23.wav")
     #loaded_model = tf.keras.models.load_model("../EchoWatch/models/pt500_model.h5")
     #live_audio_classification(loaded_model)
-
+    
     data = prepare_data(data_path, class_names, num_classes)
     train, val, test = split_data(data)
     
     
-    model = piczak_modell() 
+    model = model_optimized(num_classes, acti_func, loss_func) 
     model.summary()
-    hist = model.fit(train, epochs=20, validation_data=val)  # Reduced epochs for testing
+    callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                            patience=10,
+                                            restore_best_weights=True)
+    
+    hist = model.fit(train, epochs=100, validation_data=val)  # Reduced epochs for testing
     
     loss, accuracy, recall, precision = model.evaluate(test)
     print(f"Test Loss: {loss}")
@@ -42,51 +48,9 @@ def main_multiclass_classification(data_path, class_names, num_classes, output_f
     
     save_predictions_to_csv(model, data_path, output_filename, binary=False)
 
-def save_predictions_to_csv(model, data_path, output_filename, binary=True):
-    results = {}
-    for file in os.listdir(data_path):
-        FILEPATH = os.path.join(data_path, file)
-        wav = load_wav_16k_mono(FILEPATH)
-        
-        processed_wav = preprocess_wav_for_model(wav)
-        processed_wav_batched = tf.expand_dims(processed_wav, axis=0)
-        predictions = model.predict(processed_wav_batched)
-        
-        if binary:
-            binary_prediction = 1 if predictions[0][0] > 0.5 else 0
-            results[file] = binary_prediction
-        else:
-            predicted_class = tf.argmax(predictions, axis=1).numpy()[0]
-            print(predicted_class)
-            results[file] = predicted_class
-
-    
-    with open(output_filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Dateiname', 'Vorhersage'])  
-        for key, value in results.items():
-            writer.writerow([key,value])
-
-
-
-def predict_with_saved_model(model_path , wav_file_path):
-    
-    loaded_model = tf.keras.models.load_model(model_path)
-    
-    wav = load_wav_16k_mono(wav_file_path)
-    processed_wav = preprocess_wav_for_model(wav)
-    processed_wav_batched = tf.expand_dims(processed_wav, axis=0)
-    
-    predictions = loaded_model.predict(processed_wav_batched)
-    
-    # Return the class with the highest probability
-    #predicted_class = tf.argmax(predictions[0]).numpy()
-
-    predicted_class = tf.argmax(predictions, axis=1).numpy()[0]
-    return print(predicted_class)
 
 
 data_path = os.path.join('data', 'PT500')
-class_names=['A', 'B', 'C', 'D']
-num_classes=4
+class_names=['A', 'B', 'C', 'D', 'E', 'F']
+num_classes=6
 main_multiclass_classification(data_path, class_names, num_classes)
